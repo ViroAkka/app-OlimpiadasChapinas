68b89a323bf654ae9995d60e653614f60b6b1e7d
ramiro_mv11 <ram.martnez11@gmail.com>
2025-05-10 07:02:19 -0600

    Se agregan estilos a Pais.

--- app-OlimpiadasChapinas/JS/jsPais.js
+++ app-OlimpiadasChapinas/JS/jsPais.js
@@ -2,7 +2,7 @@
     event.preventDefault();
 
     let respuesta = "";
-    if ($("#idPais").val().trim() == "") {
+    if ($("#idPais").val() == null) {
         respuesta += "\n{ID del Pais}";
     }
 
@@ -15,4 +15,34 @@
     } else {
         $("form").submit(); // Envía el formulario si no hay errores
     }
+}
+
+const input = document.getElementById("idPais");
+const inputNombre = document.getElementById("nombre");
+
+if (input && !inputNombre) {
+    const mensaje = "Escribe el ID del País";
+    let index = 0;
+    let borrando = false;
+
+    function animarPlaceholder() {
+        if (!borrando) {
+            input.placeholder = mensaje.substring(0, index + 1);
+            index++;
+            if (index === mensaje.length) {
+                borrando = true;
+                setTimeout(animarPlaceholder, 2000);
+                return;
+            }
+        } else {
+            input.placeholder = mensaje.substring(0, index - 1);
+            index--;
+            if (index === 0) {
+                borrando = false;
+            }
+        }
+        setTimeout(animarPlaceholder, 70);
+    }
+
+    animarPlaceholder();
 }
