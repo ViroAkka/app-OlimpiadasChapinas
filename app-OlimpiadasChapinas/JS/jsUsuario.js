45d0b6165e8c72afd535f6e52a9ab0d24dcfd633
ramiro_mv11 <ram.martnez11@gmail.com>
2025-05-11 11:57:31 -0600

    Se agregan estilos a Usuario y se comienza con la creación de una ventana modal.

--- app-OlimpiadasChapinas/JS/jsUsuario.js
+++ app-OlimpiadasChapinas/JS/jsUsuario.js
@@ -67,3 +67,58 @@ function validarGuardadoDeCamposActualizacion(event) {
         $("form").submit(); // Envía el formulario si no hay errores
     }
 }
+
+const inputUsuario = document.getElementById("idUsuario");
+const inputEmail = document.getElementById("email");
+
+if (inputUsuario && inputEmail) {
+    const mensajeUsuario= "Escribe el ID del Usuario";
+    const mensajeEmail = "Escribe el Email del Usuario";
+
+    let indexUsuario= 0;
+    let borrandoUsuario= false;
+
+    let indexEmail = 0;
+    let borrandoEmail = false;
+
+    function animarPlaceholderUsuario() {
+        if (!borrandoUsuario) {
+            inputUsuario.placeholder = mensajeUsuario.substring(0, indexUsuario+ 1);
+            indexUsuario++;
+            if (indexUsuario=== mensajeUsuario.length) {
+                borrandoUsuario= true;
+                setTimeout(animarPlaceholderUsuario, 2000);
+                return;
+            }
+        } else {
+            inputUsuario.placeholder = mensajeUsuario.substring(0, indexUsuario- 1);
+            indexUsuario--;
+            if (indexUsuario=== 0) {
+                borrandoUsuario= false;
+            }
+        }
+        setTimeout(animarPlaceholderUsuario, 70);
+    }
+
+    function animarPlaceholderEmail() {
+        if (!borrandoEmail) {
+            inputEmail.placeholder = mensajeEmail.substring(0, indexEmail + 1);
+            indexEmail++;
+            if (indexEmail === mensajeEmail.length) {
+                borrandoEmail = true;
+                setTimeout(animarPlaceholderEmail, 1400);
+                return;
+            }
+        } else {
+            inputEmail.placeholder = mensajeEmail.substring(0, indexEmail - 1);
+            indexEmail--;
+            if (indexEmail === 0) {
+                borrandoEmail = false;
+            }
+        }
+        setTimeout(animarPlaceholderEmail, 70);
+    }
+
+    animarPlaceholderUsuario();
+    animarPlaceholderEmail();
+}
