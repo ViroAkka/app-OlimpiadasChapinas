b20ed98d81d4aa816258742e87aaac7d53e908cc
ramiro_mv11 <ram.martnez11@gmail.com>
2025-05-27 06:31:52 -0600

    Se realizaron cambios en las vistas.

--- app-OlimpiadasChapinas/Controllers/HomeController.cs
+++ app-OlimpiadasChapinas/Controllers/HomeController.cs
@@ -44,19 +44,5 @@ namespace app_OlimpiadasChapinas.Controllers
 
             return View(data);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
